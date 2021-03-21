87a3a609c82d9d3c62b9127cddfff1716a3c4415
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-03-21 15:59:29 +0200

    Добавьте файлы проекта.

---
+++ Lab2/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IIG.Core.FileWorkingUtils;
+
+namespace Lab2 {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
